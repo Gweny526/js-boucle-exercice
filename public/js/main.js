d9f0d0e03e8302b98bb83784e573103b817eefa2
Gweny <gweny.ds@gmail.com>
2021-10-16 14:00:24 +0200

    exo for-and-foreach

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+//  exo 1
+// let compteur = 20;
+// for (let index =0; index <= compteur; index ++){
+//     console.log(`tour numero ${compteur}`);
+//     if (index % 2 == 0){
+//         console.log("numero pair");
+//     } else{
+//         console.log("numero impair");
+//     }
+// }
+// console.log("______");
+// // exo2
+// let prenoms = ["Jules", "Céline", "Nasta", "Igor"];
+// let bjr = "bonjour "
+// prenoms.forEach(element => {
+    
+// });
+
+// autre possibilité 
+
+// FOR
+//  1
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);
+// };
+
+// for (let i = 0; i <= 20; i++) {
+//     switch(i%2 == 0) {
+//         case true:
+//             console.log(`${i} est un nombre pair`);
+//             break;
+//         case false:
+//             console.log(`${i} est un nombre impair`);
+//             break;
+//     };  
+// };
+
+//2
+// let coding = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "mexence", "zulma"]
+// console.log(coding);
+// for (let i = 0; i < coding.length; i++) {
+//     console.log(`bonjour ${coding[i]}`);
+    
+// }
+
+//3 
+// let nombre = prompt('un nombre')
+// for (let i = 0; i <= nombre; i++) {
+//     console.log(i);;
+    
+// }
+
+
+// FOREACH
+//1
+// let monTab  = ['Schaerbeek', 'Gweny', 'coding', 'formation', 'pc']
+// monTab.forEach(element => {
+//     console.log(element);
+// });
+//2
+// let coding = ["Mira", "Arthos", "Jules", "Joey", "Mad", "Céline", "Igor", "Lou", "Rey", "Nastasia"]
+
+// ## sa position
+// coding.forEach(el => {
+//     console.log(`Bonjour ${el}`);
+// });
+
+// coding.forEach((e, i) => {
+//     console.log(`Element -> ${e}\n Position -> ${i}`);
+// });
